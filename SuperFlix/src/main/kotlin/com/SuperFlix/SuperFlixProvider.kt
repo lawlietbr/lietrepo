4142da3502b46f885c8b40651ced3874150a1782
L <euluan1912@gmail.com>
2025-12-08 13:41:17 -0300

    Atualizar o SuperFlixProvider.kt

--- SuperFlix/src/main/kotlin/com/SuperFlix/SuperFlixProvider.kt
+++ SuperFlix/src/main/kotlin/com/SuperFlix/SuperFlixProvider.kt
@@ -1,12 +1,17 @@
 package com.SuperFlix
 
 import android.content.Context
+import com.lagradost.cloudstream3.extractors.Filemoon
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class SuperFlixProviderPlugin: Plugin() {
     override fun load(context: Context) {
+        // Registrar o provider principal
         registerMainAPI(SuperFlix())
+        
+        // Registrar o extractor do Filemoon
+        registerExtractorAPI(Filemoon())
     }
 }
